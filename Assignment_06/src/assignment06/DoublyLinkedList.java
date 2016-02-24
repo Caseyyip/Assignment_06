ef0567e6ded6f68342240d653f7864a191f5beda
Andy Dao <andydao10@gmail.com>
2016-02-24 01:11:27 -0700

    A lot of updates with implementation.

--- Assignment_06/src/assignment06/DoublyLinkedList.java
+++ Assignment_06/src/assignment06/DoublyLinkedList.java
@@ -26,186 +26,431 @@ import java.util.NoSuchElementException;
  */
 public class DoublyLinkedList<E> implements List<E>, Iterable<E>
 {
+	private int numberOfElementsInList; // Number of elements in List
 	private Node node; // an Node object
 	private Node head; // The head of the linkedList
 	private Node tail; // The tail of the linkedList
 	
+	private Node pre; // The element before the first item
+	private Node post; // The element after the last item
+	
+	// Iterator<Object> doublyLinkedIterator = list.iterator();
+
+	
+	public static void main(String[] args){
+		DoublyLinkedList<Object> list = new DoublyLinkedList<>();
+		for(int i = 0; i < 10; i++){
+			list.add(i, i+1);
+		}
+		list.add(8, 3);
+		printArray(list.toArray());
+		System.out.println("The number 2 shows up first first at index: " + list.indexOf(3));
+		System.out.println("The number 2 shows up very last at index: " + list.lastIndexOf(3));
+	}
 	
 	/**
 	 * Default Constructor for DoublyLinkedList
 	 */
 	public DoublyLinkedList()
 	{
-		head = null;
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addFirst(E element) {
-		node = new Node(element, head, null);
+		numberOfElementsInList = 0;
+		
+		node = new Node(null, null, null);
+		node.prev = node;
+		node.next = node;
 	}
 
 	/**
-	 * Add the object o of type E, into the very end of the DoublyLinkedList
-	 * If the head is null though, just add the object into the head since it'll be the first element.
+	 * Returns an element of type E
 	 */
 	@Override
-	public void addLast(E o) {
-		if(head == null){ // If the head is null, just add the object into the head (first index)
-			addFirst(o);
+	public Iterator<E> iterator()  {
+		return new DoublyLinkedListIterator();
+	}
+	
+	/**
+	 * An Iterator Class for DoublyLinkedLists
+	 */
+	private class DoublyLinkedListIterator implements Iterator<E>{
+		private Node current = node.next; // The node to be returned by next();
+		private Node lastUsed = null; // The last node to be returned by either previous() or next()
+									  // This will be resetted when remove() is used or if add() is used.
+		private int index = 0; // Keeps track of index for use in next() and previous(), add() and remove()
+		
+		public boolean hasNext() {
+			return index < numberOfElementsInList; // if index is less than the number of elements in the DoublyLinkedList, return true
 		}
-		else{
-			Node tempCheck = head; 
-			while(tempCheck.next != null){ // As long as the Node doesn't have a null next...
-				tempCheck = tempCheck.next; // make the tempCheck to the next Node. 
-			}								// Keep doing this until one of the Nodes has a null next
-			//tempCheck.next = new Node(o, null);	// Add the 'E o' object to the very end and declare it's next, null.
+
+		public E next() {
+			if(hasNext() == false){
+				throw new NoSuchElementException();
+			}
+			lastUsed = current; // Makes the current node and remembers that it was last used 
+								// Remember, current is already 
+			E data = current.data; // Adds the element in current node to E data
+			current = current.next; // makes the next node after the current node, THE current node.
+			index++;
+			return data; // return that data
+		}
+
+		public boolean hasPrevious() {
+			return index > 0; // if the index is greater than 0, then there is at least 1 element in the DoublyLinkedList. Return true if there is.
+		}
+
+		public E previous() {
+			if(hasPrevious() == false){ 
+				throw new NoSuchElementException(); // if hasPrevious() returns false (meaning if there is no previous node), will throw exception.
+			}
+			current = current.prev; // Takes the previous Node 
+			index--;
+			lastUsed = current; // Will set this Node to lastUsed, so that way if remove() or add() is used, it won't throw an exception.
+			return current.data;
+		}
+
+		public int nextIndex() {
+			return index;
+		}
+
+		public int previousIndex() {
+			return index-1;
+		}
+		
+		public void remove() {
+			if(lastUsed == null){
+				throw new IllegalStateException();
+			}
+			Node x = lastUsed.prev;
+			Node y = lastUsed.next;
+			x.next = y;
+			y.prev = x;
+			numberOfElementsInList--;
+			if(current == lastUsed){
+				current = y;
+			}
+			else{
+				index--;
+			}
+			lastUsed = null; // Resets lastUsed to null
+		}
+
+		public void set(E element) {
+			if(lastUsed == null){
+				throw new IllegalStateException();
+			}
+			lastUsed.data = element;
+		}
+
+		// Add element to the DoublyLinkedList
+		public void add(E element) {
+			Node x = current.prev;
+			Node y = new Node(null, null, null);
+			Node z = current;
+			
+			y.data = element;
+			
+			x.next = y;
+			y.next = z;
+			
+			z.prev = y;
+			y.prev = x;
+			numberOfElementsInList++;
+			index++;
+			lastUsed = null; // Resets lastUsed to null
 		}
 	}
 
+	
+	
+	//==============================================================================================================================//
+	
+	@Override
+	public void addFirst(E element) 
+	{
+		Node tmp = new Node(element, head, null);
+        if(head != null ) {
+        	head.prev = tmp;
+        	}
+        head = tmp;
+        if(tail == null) { 
+        	tail = tmp;
+        	}
+		
+		//add(0, element);
+	}
+	
+	@Override
+	public void addLast(E element)
+	{
+		add(size(), element);
+	}
+
+	/**
+	 * Add item to list of specified index
+	 * 
+	 * @throws IndexOutOfBoundsException
+	 */
 	@Override
 	public void add(int index, E element) throws IndexOutOfBoundsException
 	{
+		if(index < 0 || index > size()){ // If the given index is less than 0 or is bigger than the list size
+			throw new IndexOutOfBoundsException();
+		}
+		if(size() == 0){
+			addFirst(element);
+		}
+
+		addBefore(getNodeAtIndex(index), element); // Add the node at the given index. 
+												   // If there is already an element there, move that element 1 index to the right
+	}
+	
+	/**
+	 * This will add the element right before the given index in add().
+	 * Helper method that creates a node for the element and will initiate it's prev and next values
+	 */
+	public Node addBefore(Node mNode, E element){
+		Node addThisNode = new Node(null, null, null); // Create a whole new node
+		addThisNode.data = element; // Add the element to the data part of the node
+		addThisNode.prev = mNode.prev; // Take the Node that was at this index, takes its prev and declare the new added Node that prev.
+		addThisNode.next = mNode; // Now make the Node that was at this index, and make it the value of the new added Node's next.
+		addThisNode.next.prev = addThisNode; 
+		addThisNode.prev.next = addThisNode;
 		
+		numberOfElementsInList++;
+		return addThisNode;
 	}
 
+	/**
+	 * Gets the first element in the list
+	 * @throws NoSuchElementException if the head itself is null.
+	 */
 	@Override
 	public E getFirst() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(size() == 0){
+			throw new NoSuchElementException();
+		}
+		return getNodeAtIndex(0).data;
 	}
 
+	/**
+	 * Gets the last element in the list
+	 * @throws NoSuchElementException if the head is null
+	 */
 	@Override
 	public E getLast() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(size() == 0){
+			throw new NoSuchElementException();
+		}
+		return getNodeAtIndex(size()-1).data;
 	}
 
+	/**
+	 * Gets the element at the specified index given.
+	 */
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		if(index < 0 || index > size() -1){
+			throw new IndexOutOfBoundsException();
+		}
+		else{
+			return getNodeAtIndex(index).data;
+		}
+	}
+	
+	/**
+	 * Private helper method to get the node of the specified index
+	 * @returns the node at the index
+	 */
+	private Node getNodeAtIndex(int index){
+		Node getNode = new Node(null, null, null);
+		if(index < size() / 2){
+			getNode = node.next;
+				for(int j = 0; j < index; j++){
+					getNode = getNode.next;
+				}
+			}
+		else{
+			getNode = node;
+			for(int j = size(); j > index; j--){ // Start at the end of the list, and increment down until it reaches 1 more than the index given
+				getNode = getNode.prev;
+			}
+		}
+		return getNode;
 	}
 
 	@Override
 	public E removeFirst() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(size() == 0){
+			throw new NoSuchElementException();
+		}
+		return remove(0);
 	}
 
 	@Override
 	public E removeLast() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(size() == 0){
+			throw new NoSuchElementException();
+		}
+		return remove(size()-1);
 	}
 
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		if(index < 0 || index > size()-1){
+			throw new IndexOutOfBoundsException();
+		}
+		else{
+			Node rem = getNodeAtIndex(index);
+			remove(rem);
+			return rem.data; 
+		}
+	}
+	
+	/**
+	 * Helper method to remove an element
+	 */
+	public void remove(Node rNode){
+		rNode.prev.next = rNode.next;
+		rNode.next.prev = rNode.prev;
+		numberOfElementsInList--;
 	}
 
 	@Override
 	public int indexOf(E element) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int lastIndexOf(E element) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		DoublyLinkedListIterator indexOfIterator = new DoublyLinkedListIterator();
+		int index = 0;
+		
+		while(indexOfIterator.hasNext()){
+			if(indexOfIterator.next().equals(element)){
+				return index;
+			}
+			index++;
+		}
+		return -1;
 	}
 
 	/**
-	 * Checks to see if the DoublyLinkedList is empty by checking if the head is null. 
+	 * Returns the index with this element of it's last occurrence.
+	 * Returns -1 if the element is not found.
+	 */
+	@Override
+	public int lastIndexOf(E element) {
+		DoublyLinkedListIterator lastIndexIterator = new DoublyLinkedListIterator();
+		int index = numberOfElementsInList;
+		
+		while(lastIndexIterator.hasPrevious()){
+			if(lastIndexIterator.previous().equals(element)){
+				return index;
+			}
+			index--;
+		}
+		return -1;
+	}
+
+	/**
+	 * Checks to see how many elements are in the DoublyLinkedList
+	 * @return
+	 */
+	@Override
+	public int size() {
+		return numberOfElementsInList;
+	}
+
+	/**
+	 * Checks to see if the DoublyLinkedList is empty by checking if the head is null or if numberOfElementsInList is 0.
 	 * If the head is null, there are no elements.
 	 */
 	@Override
 	public boolean isEmpty() {
-		return head == null;
+		return numberOfElementsInList == 0 || head == null;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		head = null;
 	}
 
+	/**
+	 * Convert to an Array
+	 */
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		Object[] convertToArray = new Object[size()];
+		
+		for(int i = 0; i < size(); i++){
+			convertToArray[i] = getNodeAtIndex(i).data;
+		}
+		return convertToArray;
+	}
+	
+	/**
+	 * Helps to print the DoublyLinkedList 
+	 */
+	public static <E> void printArray(E[] inputArray){
+		for(E elements : inputArray){
+			System.out.printf("%s ", elements);
+		}
+		System.out.println();
 	}
 
 /**
  * Inner Class which defines the Node class.
  */
-public class Node {
+private class Node {
 	private E data;
+	private Node prev;
 	private Node next;
-	private Node previous;
 	
-	public Node(E data, Node next, Node previous){
+	
+	public Node(E data, Node next, Node prev){
 		this.data = data;
 		this.next = next;
-		this.previous = previous;
+		this.prev = prev;
 	}
 	
-	/**
-	 * Gets the data
-	 */
-	public E getData(){
-		return data;
-	}
-	
-	/**
-	 * Gets the next Node
-	 */
-	public Node getNextNode(){
-		return next;
-	}
-	
-	/**
-	 * Gets the previous Node
-	 */
-	public Node getPreviousNode()
-	{
-		return previous;
-	}
-	
-	/**
-	 * Sets the data
-	 */
-	public void setData(E mData){
-		data = mData;
-	}
-	
-	/**
-	 * Sets the next Node
-	 */
-	public void setNextNode(Node mNode)
-	{
-		next = mNode;
-		}
-	
-	/**
-	 * Sets the previous Node
-	 */
-	public void setPreviousNode(Node pNode)
-	{
-		previous = pNode;
-	}
+//	/**
+//	 * Gets the data
+//	 */
+//	public E getData(){
+//		return data;
+//	}
+//	
+//	/**
+//	 * Gets the next Node
+//	 */
+//	public Node getNextNode(){
+//		return next;
+//	}
+//	
+//	/**
+//	 * Gets the previous Node
+//	 */
+//	public Node getPreviousNode()
+//	{
+//		return prev;
+//	}
+//	
+//	/**
+//	 * Sets the data
+//	 */
+//	public void setData(E mData){
+//		data = mData;
+//	}
+//	
+//	/**
+//	 * Sets the next Node
+//	 */
+//	
+//	public void setLinkNext(Node mNode)
+//	{
+//		next = mNode;
+//	}
+//	
+//	/**
+//	 * Sets the previous Node
+//	 */
+//	public void setLinkPrevious(Node pNode)
+//	{
+//		prev = pNode;
+//	}
 }
 }
--- Assignment_06/src/assignment06/DoublyLinkedListTest.java
+++ Assignment_06/src/assignment06/DoublyLinkedListTest.java
@@ -2,133 +2,319 @@ package assignment06;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Random;
+
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import assignment06.DoublyLinkedList.Node;
+
 
 /**
  * @author Andy Dao
  */
 public class DoublyLinkedListTest {
 	
-	private Node node;
-	private Node head;
+	private Random randomInt;
+	private DoublyLinkedList<Object> doublyLinkedList;
+	private DoublyLinkedList<Object> alreadyDefinedMixedDoublyLinkedList; // a mix doubly linked list of integers and strings
 	private Integer integerObject1;
-	private DoublyLinkedList<Object> doublyLinked = new DoublyLinkedList<Object>();
+	private Integer integerObject2;
+	private Integer integerObject3;
+	private DoublyLinkedList<Object> integerList;
+	private DoublyLinkedList<Object> stringList;
+
 	
 	/**
 	 * Initiate objects in this setup method / setup test fixtures
 	 */
 	@Before
 	public void setUp() throws Exception {
+// Initiate iterator
+		
+// Initiate doublyLinkedList
+		doublyLinkedList =  new DoublyLinkedList<Object>();
+		
+// Some Integer objects
 		integerObject1 = 5;
+		integerObject2 = 20;
+		integerObject3 = 55;
+		
+// An already declared DoublyLinkedList mixed with Integers and Strings
+		alreadyDefinedMixedDoublyLinkedList = new DoublyLinkedList<Object>(); 
+		alreadyDefinedMixedDoublyLinkedList.add(0, 5);
+		alreadyDefinedMixedDoublyLinkedList.add(1, "this");
+		alreadyDefinedMixedDoublyLinkedList.add(2, 100);
+		alreadyDefinedMixedDoublyLinkedList.add(3, "is a mix");
+		
+// Integer DoublyLinkedList
+		integerList = new DoublyLinkedList<Object>();
+		
+// String DoublyLinkedList
+		stringList = new DoublyLinkedList<Object>();
 	}
 
-	
-	/**
-	 * Tests for the addFirst method
-	 */
+
+/**
+ * Tests for the addFirst() method and will check with the size of the list
+ */
 	@Test
-	public void addFirst_Test1() {
-		assertFalse(doublyLinked.isEmpty());
+	// Add just 1 Integer object 
+	public void addFirst_NoElementsAtStart_Integers() {
+		integerList.addFirst(integerObject1);
+		assertEquals(1, integerList.size());
+	}
+	
+	@Test
+	// Add just 1 String object
+	public void addFirst_NoElementsAtStart_Strings(){
+		stringList.addFirst("This is the first");
+		assertEquals(1, stringList.size());
+	}
+	
+	// Add three Integer objects
+	@Test
+	public void addFirst_ThreeElements_Integers() {
+		integerList.addFirst(integerObject1);
+		integerList.addFirst(integerObject2);
+		integerList.addFirst(integerObject3);
+		assertEquals(3, integerList.size());
+	}
+	
+	// Add three String objects
+	@Test
+	public void addFirst_ThreeElements_Strings() {
+		stringList.addFirst("One");
+		stringList.addFirst("Two Two");
+		stringList.addFirst("Three Three Three");
+		assertEquals(3, stringList.size());
 	}
 	
 	
-	/**
-	 * Tests for the addLast method
-	 */
+/**
+ * Tests for the addLast() method and will check with the size of the list
+ */
 	@Test
-	public void addLast_Test1(){
+	// Add just 1 Integer object 
+	public void addLast_NoElementsAtStart_Integers(){
+		integerList.addLast(300); // Simply just add 300 to the end of the list with 
+		assertEquals(1, integerList.size());
+	}
+	
+	@Test
+	// Add just 1 String object
+	public void addLast_NoElementsAtStart_Strings(){
+		stringList.addLast("This is the last");
+		assertEquals(1, stringList.size());
+	}
+	
+	@Test
+	public void addLast_RegularAdd_ThenAddLast_Integers(){
+		integerList.add(0, 1); // add 1 to index 0
+		integerList.add(1, 4); // add 4 to index 1
+		integerList.add(2, 2); // add 2 to index 2
+		integerList.addLast(200); // Then add 200 with addLast()
+		assertEquals(4, integerList.size()); 
+	}
+	
+	@Test
+	public void addLast_CombineAdd_AddLast_Integers(){
+		integerList.add(0, 20);
+		integerList.add(1, 15);
+		integerList.add(2, 1);
+		integerList.addLast(150);
+		integerList.add(3, 50); // add 50 to index 3, which is where 150 was
+		integerList.add(2, 1000); // Add 1000 to index 2, which is where 1 is. 1000 will be put before 1
+		assertEquals(150, integerList.getLast()); // Shall still return 150 because nothing was added in an index
+															 // past index 3, which is where 150 was
 		
+		// Uncomment printArray method below to see representation
+		// integerList.printArray(integerList.toArray());
 	}
 	
-	/**
-	 * Test for the add method
-	 */
 	@Test
-	public void add_Test1(){
-		
+	public void addLast_OnlyUseAddLast_Integers_Size(){
+		integerList.addLast(1);
+		integerList.addLast(2);
+		integerList.addLast(3);
+		integerList.addLast(4);
+		integerList.addLast(5);
+		assertEquals(5, integerList.size());
 	}
 	
-	/**
-	 * Tests for the getFirst method
-	 */
 	@Test
-	public void getFirst_Test1(){
-		
+	public void addLast_OnlyUseAddLast_Strings_Size(){
+		stringList.addLast("One");
+		stringList.addLast("Two Two");
+		stringList.addLast("Three Three Three");
+		stringList.addLast("Four Four Four Four");
+		stringList.addLast("Five Five Five Five Five");
+		assertEquals(5, stringList.size());
 	}
 	
-	/**
-	 * Tests for the getLast method
-	 */
+	
+/**
+ * Tests for the getFirst() method
+ */
 	@Test
-	public void getLast_Test1(){
-		
+	public void getFirst_Integers(){
+		integerList.add(0, 20);
+		integerList.add(1, 15);
+		integerList.add(2, 100);
+		assertEquals(20, integerList.getFirst());
 	}
 	
-	/**
-	 * Tests for the get method
-	 */
 	@Test
-	public void get_Test1(){
-		
+	public void getFirst_Strings(){
+		stringList.add(0, "Hello");
+		stringList.add(1, "Its");
+		stringList.add(2, "Me");
+		assertEquals("Hello", stringList.getFirst());
 	}
 	
-	/**
-	 * Tests for the removeFirst method
-	 */
+	@Test
+	public void getFirst_WithAddFirst_Integers(){
+		integerList.add(0, 20);
+		integerList.add(1, 15);
+		integerList.add(2, 100);
+		integerList.addFirst(90); // Add 90 to very beginning
+		assertEquals(90, integerList.getFirst());
+	}
+	
+	@Test
+	public void getFirst_WithAddFirst_Integers_mix(){
+		integerList.add(0, 20);
+		integerList.add(1, 15);
+		integerList.add(2, 100);
+		integerList.addFirst(90); // Add 90 to very beginning
+		integerList.add(0, 1555); // This will move 90 that was at the beginning
+		integerList.add(2, 644);
+		assertEquals(1555, integerList.getFirst());
+
+		// Uncomment printArray method below to see representation
+		// integerList.printArray(integerList.toArray());
+	}
+	
+	@Test
+	public void getFirst_FromMixedList(){
+		assertEquals(5, alreadyDefinedMixedDoublyLinkedList.getFirst()); // Check how this mixed list is already declared above
+	}
+	
+	
+/**
+ * Tests for the getLast method
+ */
+	@Test
+	public void getLast_Integers(){
+		doublyLinkedList.add(0, 10);
+		doublyLinkedList.add(1, 15);
+		doublyLinkedList.add(2, 25);
+		assertEquals(25, doublyLinkedList.getLast());
+	}
+	
+	@Test
+	public void getLast_Strings(){
+		doublyLinkedList.add(0, "One");
+		doublyLinkedList.add(1, "Two Two");
+		doublyLinkedList.add(2, "Three Three Three");
+		assertEquals("Three Three Three", doublyLinkedList.getLast());
+	}	
+	@Test
+	public void getLast_OnlyUseAddLast_Integers(){
+		integerList.addLast(1);
+		integerList.addLast(2);
+		integerList.addLast(3);
+		integerList.addLast(4);
+		integerList.addLast(5);
+		assertEquals(5, integerList.getLast());
+	}
+	
+	@Test
+	public void getLast_OnlyUseAddLast_Strings(){
+		stringList.addLast("One");
+		stringList.addLast("Two Two");
+		stringList.addLast("Three Three Three");
+		stringList.addLast("Four Four Four Four");
+		stringList.addLast("Five Five Five Five Five");
+		assertEquals("Five Five Five Five Five", stringList.getLast());
+	}
+	
+	
+	@Test
+	public void getLast_RegularAdd_ThenAddLast_Integers(){
+		integerList.add(0, 1); // add 1 to index 0
+		integerList.add(1, 4); // add 4 to index 1
+		integerList.add(2, 2); // add 2 to index 2
+		integerList.addLast(200); // Then add 200 with addLast()
+		assertEquals(200, integerList.getLast()); 
+	}	
+	
+	@Test
+	public void getLast_RegularAdd_ThenAddLast_Strings(){
+		integerList.add(0, "One"); // add 1 to index 0
+		integerList.add(1, "Two Two"); // add 4 to index 1
+		integerList.add(2, "Three Three Three"); // add 2 to index 2
+		integerList.addLast("Last"); // Then add 200 with addLast()
+		assertEquals("Last", integerList.getLast());
+	}	
+	
+	@Test
+	public void getLast_FromMixedList(){
+		assertEquals("is a mix", alreadyDefinedMixedDoublyLinkedList.getLast()); // Check how this mixed list is already declared above
+	}
+	
+	
+/**
+ * Tests for the removeFirst method
+ */
 	@Test
 	public void removeFirst_Test1(){
-		
 	}
 	
-	/**
-	 * Tests for the removeLast method
-	 */
+/**
+ * Tests for the removeLast method
+ */
 	@Test
 	public void removeLast_Test1(){
 		
 	}
 	
-	/**
-	 * Tests for the remove method
-	 */
+/**
+ * Tests for the remove method
+ */
 	@Test
 	public void remove_Test1(){
 		
 	}
 	
-	/**
-	 * Tests for the isEmpty method
-	 */
+/**
+ * Tests for the isEmpty method
+ */
 	@Test
 	public void isEmpty_Test1(){
 		
 	}
 	
-	/**
-	 * Tests for the clear method
-	 */
+/**
+ * Tests for the clear method
+ */
 	@Test
 	public void clear_Test1(){
 		
 	}
 	
-	/**
-	 * Tests for the toArray method
-	 */
+/**
+ * Tests for the toArray method
+ */
 	@Test
 	public void toArray_Test1(){
 		
 	}
 	
-	/**
-	 * Releases external resources that was done in the @before class
-	 */
+/**
+ * Releases external resources that was done in the @before class
+ */
 	@After
 	public void tearDown() throws Exception {
 	}
